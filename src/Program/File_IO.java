cba7ab743a1e5d57f338e5dc4d27dd5761c758ea
Luis Vargas Tamayo <31453575+FatherLuis@users.noreply.github.com>
2018-10-16 17:51:13 -0500

    Update File_IO.java

--- src/Program/File_IO.java
+++ src/Program/File_IO.java
@@ -5,7 +5,20 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-
+/*******************************************************************************
+***CLASS NAME: File_IO
+***CLASS AUTHOR: LUIS E VARGAS TAMAYO
+********************************************************************************
+***PURPOSE OF CLASS: READ CVS FILE AND COLLECT INFORMATION
+********************************************************************************
+***DATE: 13 OCTOBER, 2018
+********************************************************************************
+***LIST OF CHANGES WITH DATES: NONE
+********************************************************************************
+***SPECIAL NOTES: NONE
+*** 
+***
+*******************************************************************************/
 public class File_IO 
 {
     
@@ -13,7 +26,17 @@ public class File_IO
     private ArrayList CityOperation;
     private HashTable HT;
     
-    
+    /***************************************************************************
+    ***METHOD NAME: File_IO()
+    ***METHOD AUTHOR: LUIS E VARGAS TAMAYO
+    ****************************************************************************
+    ***PURPOSE OF THE METHOD: CONSTRUCTOR
+    ***METHOD USED: NONE
+    ***METHOD PARAMETERS: NONE
+    ***RETURN VALUE: NONE
+    ****************************************************************************
+    ***DATE: 13 OCTOBER, 2018
+    ***************************************************************************/    
     public File_IO()
     {
         CityList = new ArrayList();
@@ -21,6 +44,17 @@ public class File_IO
         HT = new HashTable();
     }
     
+    /***************************************************************************
+    ***METHOD NAME: ReadFile()
+    ***METHOD AUTHOR: LUIS E VARGAS TAMAYO
+    ****************************************************************************
+    ***PURPOSE OF THE METHOD: READS CVS FILE, COLLECTS INFORMATION, AND STORES
+    ***METHOD USED: SetUp()
+    ***METHOD PARAMETERS: NONE
+    ***RETURN VALUE: NONE
+    ****************************************************************************
+    ***DATE: 13 OCTOBER, 2018
+    ***************************************************************************/    
     public void ReadFile()
     {
         
@@ -52,6 +86,17 @@ public class File_IO
 
     }
     
+    /***************************************************************************
+    ***METHOD NAME: SetUp()
+    ***METHOD AUTHOR: LUIS E VARGAS TAMAYO
+    ****************************************************************************
+    ***PURPOSE OF THE METHOD: ANALYZE RAW DATA AND COLLECT WHAT IS NEEDED
+    ***METHOD USED: NONE
+    ***METHOD PARAMETERS: STRING, STRING, STRING
+    ***RETURN VALUE: NONE
+    ****************************************************************************
+    ***DATE: 13 OCTOBER, 2018
+    ***************************************************************************/    
     private void SetUp(String C1, String C2, String Weight)
     {
         if(!isList(C1))
@@ -67,6 +112,17 @@ public class File_IO
         HT.NewData(GetIndex(C1), GetIndex(C2), Double.parseDouble(Weight));  
     }
     
+    /***************************************************************************
+    ***METHOD NAME: isList()
+    ***METHOD AUTHOR: LUIS E VARGAS TAMAYO
+    ****************************************************************************
+    ***PURPOSE OF THE METHOD: CHECKS IF STRING IS IN THE ARRALIST
+    ***METHOD USED: NONE
+    ***METHOD PARAMETERS: STRING
+    ***RETURN VALUE: BOOLEAN
+    ****************************************************************************
+    ***DATE: 13 OCTOBER, 2018
+    ***************************************************************************/    
     private boolean isList(String X)
     {
         for(int i =0; i < this.CityList.size(); i++)
@@ -79,6 +135,17 @@ public class File_IO
         return false;   
     }
     
+    /***************************************************************************
+    ***METHOD NAME: GetIndex()
+    ***METHOD AUTHOR: LUIS E VARGAS TAMAYO
+    ****************************************************************************
+    ***PURPOSE OF THE METHOD: GET THE INDEX OF A STRING FOUND IN THE ARRAYLIST
+    ***METHOD USED: NONE
+    ***METHOD PARAMETERS: STRING
+    ***RETURN VALUE: INT
+    ****************************************************************************
+    ***DATE: 13 OCTOBER, 2018
+    ***************************************************************************/    
     private int GetIndex(String X)
     {
         int j = 0;
@@ -93,11 +160,43 @@ public class File_IO
         return j; 
     }
     
-    
+    /***************************************************************************
+    ***METHOD NAME: getCityList()
+    ***METHOD AUTHOR: LUIS E VARGAS TAMAYO
+    ****************************************************************************
+    ***PURPOSE OF THE METHOD: RETURN VARIABLE CITYLIST
+    ***METHOD USED: NONE
+    ***METHOD PARAMETERS: NONE
+    ***RETURN VALUE: NONE
+    ****************************************************************************
+    ***DATE: 13 OCTOBER, 2018
+    ***************************************************************************/
     public ArrayList getCityList(){return this.CityList;}
-    
+        
+    /***************************************************************************
+    ***METHOD NAME: getCityOperation()
+    ***METHOD AUTHOR: LUIS E VARGAS TAMAYO
+    ****************************************************************************
+    ***PURPOSE OF THE METHOD: RETURN VARIABLE CITYOPERATION
+    ***METHOD USED: NONE
+    ***METHOD PARAMETERS: NONE
+    ***RETURN VALUE: NONE
+    ****************************************************************************
+    ***DATE: 13 OCTOBER, 2018
+    ***************************************************************************/
     public ArrayList getCityOperation(){return this.CityOperation;}
-    
+        
+    /***************************************************************************
+    ***METHOD NAME: getHashTable()
+    ***METHOD AUTHOR: LUIS E VARGAS TAMAYO
+    ****************************************************************************
+    ***PURPOSE OF THE METHOD: RETURN HASHTABLE
+    ***METHOD USED: NONE
+    ***METHOD PARAMETERS: NONE
+    ***RETURN VALUE: NONE
+    ****************************************************************************
+    ***DATE: 13 OCTOBER, 2018
+    ***************************************************************************/
     public HashTable getHashTable(){return HT;}
     
 }
